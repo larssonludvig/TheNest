21c906b6e8bb352471700e40c97f140dd09a571b
Ludvig Larsson <c20lln@cs.umu.se@>
2024-06-04 23:12:32 +0200

    Fixed options functionality and design

---
+++ Components/Constants.cs
@@ -0,0 +1,16 @@
+public class Constants {
+    public List<string> classTypes = new List<string> {"Light", "Medium", "Heavy"};
+
+    public List<string> lSpecializationTypes = new List<string> {"Cloaking Device", "Evasive Dash", "Grappling Hook"};
+    public List<string> lWeaponTypes = new List<string> {"V95", "93R", "M11", "XP-54", "SH1900", "LH1", "SR-84", "Dagger", "Sword", "Throwing Knives"};
+    public List<string> lGadgetTypes = new List<string> {"Breach Charge", "Flashbang", "Frag Grenade", "Gas Grenade", "Gateway", "Goo Grenade", "Glitch Grenade", "Smoke Grenade", "Sonar Grenade", "Stun Gun", "Thermal Vision", "Tracking Dart", "Pyro Grenade", "Vanishing Bomb"};
+    
+    public List<string> mSpecializationTypes = new List<string> {"Guardian Turret", "Healing Beam", "Dematerialiser"};
+    public List<string> mWeaponTypes = new List<string> {"AKM", "FCAR", "CL-40", "R.357", "Model 1887", "Riot Shield", "FAMAS"};
+    public List<string> mGadgetTypes = new List<string> {"APS Turret", "Defibrillator", "Explosive Mine", "Flashbang", "Frag Grenade", "Gas Grenade", "Gas Mine", "Glitch Trap", "Goo Grenade", "Jump Pad", "Incendiary Grenade", "Sonar Grenade", "Tracking Dart", "Zipline", "Night Vision", "Data Reshaper"};
+
+    public List<string> hSpecializationTypes = new List<string> {"Charge 'n' Slam", "Goo Gun", "Mesh Shield"};
+    public List<string> hWeaponTypes = new List<string> {"M60", "Lewis", "Flamethrower", "MGL32", "SA1216", "Sledgehammer", "KS-23"};
+    public List<string> hGadgetTypes = new List<string> {"Frag Grenade", "Flashbang", "Incendiary Grenade", "Goo Grenade", "Gas Grenade", "C4", "Explosive Mine", "Incendiary Mine", "Barricade", "RPG-7", "Dome Shield", "Night Vision", "Anti-Gravity Cube"};
+
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddScoped<CookieService>();
+builder.Services.AddScoped<Constants>();
 
 var app = builder.Build();
 
