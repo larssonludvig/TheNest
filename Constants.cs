6fd3852d32a4bb2a45bd61393f2eb4a2987a5282
Ludvig Larsson <c20lln@cs.umu.se@>
2024-06-06 10:35:33 +0200

    Added images for most items

--- Constants.cs
+++ Constants.cs
@@ -1,16 +1,16 @@
 public class Constants {
     public List<string> classTypes = new List<string> {"Light", "Medium", "Heavy"};
 
-    public List<string> lSpecializationTypes = new List<string> {"Cloaking Device", "Evasive Dash", "Grappling Hook"};
-    public List<string> lWeaponTypes = new List<string> {"V95", "93R", "M11", "XP-54", "SH1900", "LH1", "SR-84", "Dagger", "Sword", "Throwing Knives"};
-    public List<string> lGadgetTypes = new List<string> {"Breach Charge", "Flashbang", "Frag Grenade", "Gas Grenade", "Gateway", "Goo Grenade", "Glitch Grenade", "Smoke Grenade", "Sonar Grenade", "Stun Gun", "Thermal Vision", "Tracking Dart", "Pyro Grenade", "Vanishing Bomb"};
+    public List<string> lSpecializationTypes = new List<string> {"Cloaking_Device", "Evasive_Dash", "Grappling_Hook"};
+    public List<string> lWeaponTypes = new List<string> {"V9S", "93R", "M11", "XP-54", "SH1900", "LH1", "SR-84", "Dagger", "Sword", "Throwing_Knives"};
+    public List<string> lGadgetTypes = new List<string> {"Breach_Charge", "Flashbang", "Frag_Grenade", "Gas_Grenade", "Gateway", "Goo_Grenade", "Glitch_Grenade", "Smoke_Grenade", "Sonar_Grenade", "Stun_Gun", "Thermal_Vision", "Tracking_Dart", "Pyro_Grenade", "Vanishing_Bomb"};
     
-    public List<string> mSpecializationTypes = new List<string> {"Guardian Turret", "Healing Beam", "Dematerialiser"};
-    public List<string> mWeaponTypes = new List<string> {"AKM", "FCAR", "CL-40", "R.357", "Model 1887", "Riot Shield", "FAMAS"};
-    public List<string> mGadgetTypes = new List<string> {"APS Turret", "Defibrillator", "Explosive Mine", "Flashbang", "Frag Grenade", "Gas Grenade", "Gas Mine", "Glitch Trap", "Goo Grenade", "Jump Pad", "Incendiary Grenade", "Sonar Grenade", "Tracking Dart", "Zipline", "Night Vision", "Data Reshaper"};
+    public List<string> mSpecializationTypes = new List<string> {"Guardian_Turret", "Healing_Beam", "Dematerializer"};
+    public List<string> mWeaponTypes = new List<string> {"AKM", "FCAR", "CL-40", "R.357", "Model_1887", "Riot_Shield", "FAMAS"};
+    public List<string> mGadgetTypes = new List<string> {"APS_Turret", "Defibrillator", "Explosive_Mine", "Flashbang", "Frag_Grenade", "Gas_Grenade", "Gas_Mine", "Glitch_Trap", "Goo_Grenade", "Jump_Pad", "Pyro_Grenade", "Sonar_Grenade", "Tracking_Dart", "Zipline", "Data_Reshaper"};
 
-    public List<string> hSpecializationTypes = new List<string> {"Charge 'n' Slam", "Goo Gun", "Mesh Shield"};
-    public List<string> hWeaponTypes = new List<string> {"M60", "Lewis", "Flamethrower", "MGL32", "SA1216", "Sledgehammer", "KS-23"};
-    public List<string> hGadgetTypes = new List<string> {"Frag Grenade", "Flashbang", "Incendiary Grenade", "Goo Grenade", "Gas Grenade", "C4", "Explosive Mine", "Incendiary Mine", "Barricade", "RPG-7", "Dome Shield", "Night Vision", "Anti-Gravity Cube"};
+    public List<string> hSpecializationTypes = new List<string> {"Charge_N_Slam", "Goo_Gun", "Mesh_Shield"};
+    public List<string> hWeaponTypes = new List<string> {"M60", "Lewis_Gun", "Flamethrower", "MGL32", "SA1216", "Sledgehammer", "KS-23"};
+    public List<string> hGadgetTypes = new List<string> {"Frag_Grenade", "Flashbang", "Pyro_Grenade", "Goo_Grenade", "Gas_Grenade", "C4", "Explosive_Mine", "Pyro_Mine", "Barricade", "RPG-7", "Dome_Shield", "Anti-Gravity_Cube", "Motion_Sensor"};
 
 }
