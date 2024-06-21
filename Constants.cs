5b8f6b620bb87aaef24592d2fe95ab00594ace43
Ludvig Larsson <c20lln@cs.umu.se@>
2024-06-21 22:40:13 +0200

    s3 item images

--- Constants.cs
+++ Constants.cs
@@ -2,11 +2,11 @@ public class Constants {
     public List<string> Classes = new List<string> {"Light", "Medium", "Heavy"};
 
     public List<string> LSpecializations = new List<string> {"Cloaking_Device", "Evasive_Dash", "Grappling_Hook"};
-    public List<string> LWeapons = new List<string> {"V9S", "93R", "M11", "XP-54", "SH1900", "LH1", "SR-84", "Dagger", "Sword", "Throwing_Knives", "Recursive_Bow"};
+    public List<string> LWeapons = new List<string> {"V9S", "93R", "M11", "XP-54", "SH1900", "LH1", "SR-84", "Dagger", "Sword", "Throwing_Knives", "Recurve_Bow"};
     public List<string> LGadgets = new List<string> {"Breach_Charge", "Flashbang", "Frag_Grenade", "Gas_Grenade", "Gateway", "Goo_Grenade", "Glitch_Grenade", "Smoke_Grenade", "Sonar_Grenade", "Stun_Gun", "Thermal_Vision", "Tracking_Dart", "Pyro_Grenade", "Vanishing_Bomb", "Thermal_Bore"};
     
     public List<string> MSpecializations = new List<string> {"Guardian_Turret", "Healing_Beam", "Dematerializer"};
-    public List<string> MWeapons = new List<string> {"AKM", "FCAR", "CL-40", "R.357", "Model_1887", "Riot_Shield", "FAMAS", "Dual_Katanas"};
+    public List<string> MWeapons = new List<string> {"AKM", "FCAR", "CL-40", "R.357", "Model_1887", "Riot_Shield", "FAMAS", "Dual_Blades"};
     public List<string> MGadgets = new List<string> {"APS_Turret", "Defibrillator", "Explosive_Mine", "Flashbang", "Frag_Grenade", "Gas_Grenade", "Gas_Mine", "Glitch_Trap", "Goo_Grenade", "Jump_Pad", "Pyro_Grenade", "Sonar_Grenade", "Tracking_Dart", "Zipline", "Data_Reshaper"};
 
     public List<string> HSpecializations = new List<string> {"Charge_N_Slam", "Goo_Gun", "Mesh_Shield", "Winch_Claw"};
