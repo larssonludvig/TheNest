92b77dd409337d6930537208990cc7b93c317145
Ludvig Larsson <c20lln@cs.umu.se@>
2024-08-06 23:01:35 +0200

    inital commit of maps subpage. Seoul map added

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TheNest;
 using Components.ApiService;
+using BlazorPanzoom;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<CookieService>();
 builder.Services.AddScoped<Constants>();
 builder.Services.AddScoped<ApiService>();
+builder.Services.AddBlazorPanzoomServices();
 
 
 await builder.Build().RunAsync();
