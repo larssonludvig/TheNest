0051ff639a5a8a567acfa14004de014b7d55b86e
larssonludvig <ludviglarsson01@live.se>
2024-10-04 11:23:17 +0200

    s4 new items added

--- Constants.cs
+++ Constants.cs
@@ -2,14 +2,14 @@ public class Constants {
     public List<string> Classes = new List<string> {"Light", "Medium", "Heavy"};
 
     public List<string> LSpecializations = new List<string> {"Cloaking_Device", "Evasive_Dash", "Grappling_Hook"};
-    public List<string> LWeapons = new List<string> {"V9S", "93R", "M11", "XP-54", "SH1900", "LH1", "SR-84", "Dagger", "Sword", "Throwing_Knives", "Recurve_Bow"};
+    public List<string> LWeapons = new List<string> {"V9S", "93R", "M11", "XP-54", "SH1900", "LH1", "SR-84", "Dagger", "Sword", "Throwing_Knives", "Recurve_Bow", "M26_Matter"};
     public List<string> LGadgets = new List<string> {"Breach_Charge", "Flashbang", "Frag_Grenade", "Gas_Grenade", "Gateway", "Goo_Grenade", "Glitch_Grenade", "Smoke_Grenade", "Sonar_Grenade", "Stun_Gun", "Thermal_Vision", "Tracking_Dart", "Pyro_Grenade", "Vanishing_Bomb", "Thermal_Bore"};
     
     public List<string> MSpecializations = new List<string> {"Guardian_Turret", "Healing_Beam", "Dematerializer"};
-    public List<string> MWeapons = new List<string> {"AKM", "FCAR", "CL-40", "R.357", "Model_1887", "Riot_Shield", "FAMAS", "Dual_Blades"};
+    public List<string> MWeapons = new List<string> {"AKM", "FCAR", "CL-40", "R.357", "Model_1887", "Riot_Shield", "FAMAS", "Dual_Blades", "Pike-556"};
     public List<string> MGadgets = new List<string> {"APS_Turret", "Defibrillator", "Explosive_Mine", "Flashbang", "Frag_Grenade", "Gas_Grenade", "Gas_Mine", "Glitch_Trap", "Goo_Grenade", "Jump_Pad", "Pyro_Grenade", "Zipline", "Data_Reshaper"};
 
     public List<string> HSpecializations = new List<string> {"Charge_N_Slam", "Goo_Gun", "Mesh_Shield", "Winch_Claw"};
-    public List<string> HWeapons = new List<string> {"M60", "Lewis_Gun", "Flamethrower", "MGL32", "SA1216", "Sledgehammer", "KS-23", "Spear"};
+    public List<string> HWeapons = new List<string> {"M60", "Lewis_Gun", "Flamethrower", "MGL32", "SA1216", "Sledgehammer", "KS-23", "Spear", "50_Akimbo"};
     public List<string> HGadgets = new List<string> {"Frag_Grenade", "Flashbang", "Pyro_Grenade", "Goo_Grenade", "Gas_Grenade", "C4", "Explosive_Mine", "Pyro_Mine", "Barricade", "RPG-7", "Dome_Shield", "Anti-Gravity_Cube", "Motion_Sensor"};
 }
